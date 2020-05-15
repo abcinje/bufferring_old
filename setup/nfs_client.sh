1137f60f36d638b78dc069215a77228f571fa6e3
Injae Kang <abcinje@gmail.com>
2020-05-15 18:02:31 +0000

    Create nfs scripts

---
+++ setup/nfs_client.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt install -y nfs-common
+
+mkdir $HOME/cloud
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <server_ip>" >&2
+	exit 1
+fi
+echo "***Warning: NFS mount may fail due to the inbound rules of the server***"
+sudo mount -t nfs $1:$HOME/cloud $HOME/cloud
