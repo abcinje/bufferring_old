9300fcefb79473df6d34fa6cdb1a2532def81d32
Injae Kang <abcinje@gmail.com>
2020-05-18 21:13:42 +0900

    Update setup scripts

--- setup/container/master.sh
+++ setup/container/master.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 SSH=$HOME/.ssh
+mkdir -p $SSH
 
 echo "Host *" >> $SSH/config
 echo "    ForwardAgent yes" >> $SSH/config
 echo "Host *" >> $SSH/config
 echo "    StrictHostKeyChecking no" >> $SSH/config
 
-ssh-keygen -t rsa -f $SSH/id_rsa <<< y
+ssh-keygen -t rsa -N "" -f $SSH/id_rsa <<< y
 chmod 600 $SSH/id_rsa
 cp $SSH/id_rsa.pub $HOME/cloud
+
+cat $HOME/cloud/id_rsa.pub >> $SSH/authorized_keys
--- setup/nfs/client.sh
+++ setup/nfs/client.sh
@@ -7,5 +7,4 @@ if [ $# -lt 1 ]; then
 	echo "Usage: $0 <server_ip>" >&2
 	exit 1
 fi
-echo "***Warning: NFS mount may fail due to the inbound rules of the server***"
 sudo mount -t nfs $1:$HOME/cloud $HOME/cloud
