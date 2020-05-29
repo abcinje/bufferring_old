e545a730c7c5b3b8d702992981c4d7785f33ea03
Injae Kang <abcinje@gmail.com>
2020-05-29 08:15:48 +0000

    Create a mpi library

---
+++ bufferring/libmpi.py
@@ -0,0 +1,18 @@
+from mpi4py import MPI
+import ctypes
+import os
+
+_libdir = os.path.dirname(os.path.abspath(__file__)) + '/libmpi'
+_lib = ctypes.cdll.LoadLibrary(os.path.join(_libdir, 'libmpi.so'))
+
+if MPI._sizeof(MPI.Comm) == ctypes.sizeof(ctypes.c_int):
+    MPI_Comm = ctypes.c_int
+else:
+    MPI_Comm = ctypes.c_void_p
+
+comm = MPI.COMM_WORLD
+
+def init(comm):
+    comm_ptr = MPI._addressof(comm)
+    comm_val = MPI_Comm.from_address(comm_ptr)
+    _lib.init(comm_val)
