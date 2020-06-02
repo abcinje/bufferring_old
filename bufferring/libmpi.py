ffa015807b48543ab7c1fbcfbe13874aaadb47bd
Injae Kang <abcinje@gmail.com>
2020-06-02 12:22:50 +0000

    Add queues

--- bufferring/libmpi.py
+++ bufferring/libmpi.py
@@ -12,10 +12,14 @@ else:
 
 comm = MPI.COMM_WORLD
 
-def init(comm):
+def init(comm, names):
+    bytes_list = [bytes(name, 'utf-8') for name in names]
+    arr = (ctypes.c_char_p * len(names))()
+    arr[:] = bytes_list
+
     comm_ptr = MPI._addressof(comm)
     comm_val = MPI_Comm.from_address(comm_ptr)
-    _lib.init(comm_val)
+    _lib.init(comm_val, arr, len(names))
 
 # Do not call this function
 def deinit():
